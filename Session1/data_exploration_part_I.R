fe0c43fb1b92189910961d0eab5c0f993a6526b8
Irina Chelysheva <irina.chelysheva@paediatrics.ox.ac.uk>
2024-06-18 21:45:17 +0100

    Update data_exploration_part_I.R

--- Session1/data_exploration_part_I.R
+++ Session1/data_exploration_part_I.R
@@ -1,5 +1,5 @@
 ###R for biologists
-##Irina & Rao, 24/04/2024
+##Irina & Rao, 19/06/2024
 
 # install.packages("tidyverse") # just need to do this once
 # install.packages("dplyr") # just need to do this once; also no need to do this 
