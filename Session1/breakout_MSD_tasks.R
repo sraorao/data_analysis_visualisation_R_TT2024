98e9983936526ece8959ce235d0582c9a7883a89
Irina Chelysheva <irina.chelysheva@paediatrics.ox.ac.uk>
2024-06-18 21:45:50 +0100

    Update breakout_MSD_tasks.R

--- Session1/breakout_MSD_tasks.R
+++ Session1/breakout_MSD_tasks.R
@@ -1,5 +1,5 @@
 ###R for biologists
-##Irina & Rao, 24/04/2024
+##Irina & Rao, 19/06/2024
 
 ##Breakout tasks - MSD COVID dataset
 #1) Import the corresponding sheet from Excel file provided (use readxl)
